cc8dcb31872eac3a65e3bfe68a23c2f803fa5e58
Anas Semesmieh <anas@Anass-MacBook-Pro.local>
2018-06-04 01:21:53 +0800

    Adding Code Comments

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 const readline = require('readline');
 
+//Global Robot JSON to hold the state of the robot
 var robot = { "x": 0, "y": 0, "f": "north", "placed": false }
+//Global Array of directions on the board
 var direction = ["north", "east", "south", "west"]
 
+//Initilaization of the console
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+//Input Action
 rl.on('line', (input) => {
-  var cmd = input.toLowerCase()
+  var cmd = input.toLowerCase() // to handle miss spells of upper/lower case (Standrise Commands to lower cases)
   this.commandHandler(cmd)
 });
 
+//Exported to use externally from test scripts
 module.exports.commandHandler = (command) => {
+  //RegX to compare against a valid placement command which is "place" followed by x and y bound from 0-4 acording to the board size and followed by the orientation
   var placeRGX = command.match(/^place [0-4],[0-4],(?:north|south|east|west)$/)
   
+  //Temporary storage of the command value
   var cmd = command
 
+  //Checking if there is a Regx matching and updating the value to switch against acordingly.
   if (placeRGX != null) {
     cmd = "placed"
   }
 
+//Checks for placement or ignoring commands if there is no placement
   if (robot.placed || cmd == 'placed') {
     switch (cmd) {
       case 'placed':
@@ -39,6 +48,7 @@ module.exports.commandHandler = (command) => {
       case 'report':
         report()
         break
+      //reportJSON is used in test scripts
       case 'reportJSON':
         return reportJSON()
       default:
@@ -62,7 +72,7 @@ function place(command) {
 }
 
 function move() {
-  var unChanged = true
+  var unChanged = true //Used to show a message when a move command was not executed
   switch (robot.f) {
     case 'north':
       if (robot.y < 4) {
